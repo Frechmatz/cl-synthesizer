be04e5cddea31349b2b718dad43b6d19b8340f0e
Oliver Voeltz <frechmatz@gmx.de>
2017-10-01 23:57:21 +0200

    Cleanup

--- test/synthesizer/test-assembly.lisp
+++ test/synthesizer/test-assembly.lisp
@@ -38,20 +38,3 @@
       (assert-equal "Module 1" (cl-synthesizer::get-rack-module-name found-module-1))
       ))))
 
-
-#|
-(define-test pool-stopping-worker-thread ()
-  "Test that pool cannot be stopped by a worker thread"
-  (let ((pool (cl-threadpool:make-threadpool 5)))
-    (cl-threadpool:start pool)
-    (let ((got-error nil))
-      (cl-threadpool:add-job
-       pool
-       (lambda ()
-         (handler-case
-             (cl-threadpool:stop pool)
-           (error (err) (setf got-error err)))))
-      (cl-threadpool:stop pool)
-      (assert-true got-error)
-      (assert-true (typep got-error 'cl-threadpool:threadpool-error)))))
-|#
