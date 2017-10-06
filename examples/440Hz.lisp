190fd853781ae9ed8e3b6e224c796471333e7a29
Oliver Voeltz <frechmatz@gmx.de>
2017-10-06 20:22:24 +0200

    Added 440Hz mono example

--- examples/440Hz.lisp
+++ examples/440Hz.lisp
@@ -2,8 +2,8 @@
 (in-package :cl-synthesizer-examples)
 
 
-(defun synthesizer-example-440hz ()
-  "Write two 440Hz channels into 440HzExample.wav"
+(defun synthesizer-example-440hz-two-channel ()
+  "Write two 440Hz channels"
   (let ((rack (make-instance 'cl-synthesizer:rack)))
     (cl-synthesizer::add-module rack (cl-synthesizer::make-rack-module
 				      "VCO-1"
@@ -16,11 +16,30 @@
     (cl-synthesizer::add-module rack (cl-synthesizer::make-rack-module
 				      "WAVE-WRITER"
 				      #'cl-synthesizer-modules::two-channel-wave-file-writer
-				      :filename "/Users/olli/440HzExample.wav"))
+				      :filename "/Users/olli/waves/440HzTwoChannelExample.wav"))
     (cl-synthesizer::add-patch rack "VCO-1" :out "WAVE-WRITER" :channel-1)
     (cl-synthesizer::add-patch rack "VCO-2" :out "WAVE-WRITER" :channel-2)
     (dotimes (i 100000)
       (cl-synthesizer::update-rack rack))
     (cl-synthesizer::shutdown-rack rack))))
 
-;; (synthesizer-example-440hz)
+;; (synthesizer-example-440hz-two-channel)
+
+
+(defun synthesizer-example-440hz-one-channel ()
+  "Write one 440Hz channel"
+  (let ((rack (make-instance 'cl-synthesizer:rack)))
+    (cl-synthesizer::add-module rack (cl-synthesizer::make-rack-module
+				      "VCO-1"
+				      #'cl-synthesizer-modules::cosinus-vco
+				      :f_0 440))
+    (cl-synthesizer::add-module rack (cl-synthesizer::make-rack-module
+				      "WAVE-WRITER"
+				      #'cl-synthesizer-modules::one-channel-wave-file-writer
+				      :filename "/Users/olli/waves/440HzOneChannelExample.wav"))
+    (cl-synthesizer::add-patch rack "VCO-1" :out "WAVE-WRITER" :channel-1)
+    (dotimes (i 100000)
+      (cl-synthesizer::update-rack rack))
+    (cl-synthesizer::shutdown-rack rack))))
+
+;; (synthesizer-example-440hz-one-channel)
