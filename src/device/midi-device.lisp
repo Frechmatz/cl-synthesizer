3819c58ba4985101eba079d372cf1670becf62ff
Oliver Voeltz <frechmatz@gmx.de>
2018-01-17 21:20:32 +0100

    Midi event types, simple note mapping

--- src/device/midi-device.lisp
+++ src/device/midi-device.lisp
@@ -18,7 +18,21 @@
      :cc ;; for now listen to all control change events
      (lambda (chan control value)
        (format t "[CC] Channel: ~d  Control: ~d  Value: ~d~%" chan control value)       
-       (queues:qpush event-queue (list chan control value))))
+       (queues:qpush event-queue (list :cc chan control value))))
+    (midi:set-midi-callback
+     (midi:get-source *source*)
+     :note-on
+     (lambda (chan note vel)
+       ;; [NOTE-ON] Channel: 1  Notenum: 61  Velocity: 15
+       (format t "[NOTE-ON ] Channel: ~d  Notenum: ~d  Velocity: ~d~%" chan note vel)
+       (queues:qpush event-queue (list :note-on chan note vel))))
+    (midi:set-midi-callback
+     (midi:get-source *source*)
+     :note-off
+     (lambda (chan note vel)
+       ;; [NOTE-OFF] Channel: 1  Notenum: 61  Velocity: 0
+       (format t "[NOTE-OFF] Channel: ~d  Notenum: ~d  Velocity: ~d~%" chan note vel)
+       (queues:qpush event-queue (list :note-off chan note vel))))
     (list
      :shutdown (lambda () nil)
      :inputs (lambda () '())
--- src/modules/midi-interface.lisp
+++ src/modules/midi-interface.lisp
@@ -7,18 +7,61 @@
 ;; Work in progress
 ;;
 
+;;
+;;
+;; Note Mapping
+;;
+;;
+
+;; http://subsynth.sourceforge.net/midinote2freq.html
+(defun note-frequency-table ()
+  (let ((arr (make-array 128)) (a 440))
+    (dotimes (x 128)
+      (let ((frequency
+	     (* (/ a 32) (expt 2 (/ (+ x -9) 12)))))
+	(setf (aref arr x) frequency)))
+    arr))
+
+(defun frequency-cv-table (frequency-table)
+  (let* ((arr (make-array 128))
+	 (converter (cl-synthesizer-core:linear-converter :input-min 0 :input-max 5.0 :output-min 0 :output-max 13000))
+	 (fn (getf converter :output-to-input)))
+    (dotimes (x 128)
+      (setf (aref arr x) (funcall fn (elt frequency-table x))))
+    arr))
+
+;;
+;; MIDI Module
+;;
 
 (defun midi-interface (environment)
   (declare (ignore environment))
-  (let ((current-output 0)
-	(converter (cl-synthesizer-core:linear-converter :input-min 0 :input-max 127 :output-min 0 :output-max 4.9)))
+  (let ((current-controller 0)
+	(current-gate 0)
+	(current-cv 0)
+	(note-to-cv-table (frequency-cv-table (note-frequency-table)))
+	(controller-converter (cl-synthesizer-core:linear-converter :input-min 0 :input-max 127 :output-min 0 :output-max 4.9)))
     (list
      :shutdown (lambda () nil)
      :inputs (lambda () '(:midi-event))
-     :outputs (lambda () '(:out-1))
+     :outputs (lambda () '(:gate :cv :out-1))
      :get-output (lambda (output)
-		   (declare (ignore output))
-		   current-output)
+		   (cond
+		     ((eq output :gate) current-gate)
+		     ((eq output :cv) current-cv)
+		     ((eq output :out-1) current-controller)
+		     (t (error (format nil "Unknown input ~a requested from MIDI-INTERFACE" output)))))
      :update (lambda (&key (midi-event nil))
-	       (if midi-event (setf current-output (funcall (getf converter :input-to-output) (third midi-event))))))))
-
+	       (if midi-event
+		   (let ((event-type (first midi-event)))
+		     (cond
+		       ((eq event-type :cc)
+			(setf current-controller
+			      (funcall (getf controller-converter :input-to-output) (fourth midi-event))))
+		       ((eq event-type :note-on)
+			(setf current-gate 5.0)
+			(setf current-cv
+			      (elt note-to-cv-table (third midi-event))))
+		       ((eq event-type :note-off)
+			(setf current-gate 0)))
+		     (format t "Gate: ~a CV: ~a Controller: ~a~%" current-gate current-cv current-controller)))))))
