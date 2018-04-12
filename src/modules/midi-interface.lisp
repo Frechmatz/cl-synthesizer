31baeece1fd1ef37d50aef24a42a33bf015cb3c1
Oliver Voeltz <frechmatz@gmx.de>
2018-04-12 20:20:43 +0200

    Added unisono play mode to midi interface

--- src/modules/midi-interface.lisp
+++ src/modules/midi-interface.lisp
@@ -17,14 +17,14 @@
 (defconstant +voice-state-gate+ 1)
 (defconstant +voice-state-gate-on-logger+ 2)
 (defconstant +voice-state-gate-off-logger+ 3)
-(defconstant +voice-state-gate-trigger+ 4)
+(defconstant +voice-state-gate-retrigger+ 4)
 
 (defparameter +voice-states+
   '(+voice-state-cv+
     +voice-state-gate+
     +voice-state-gate-on-logger+
     +voice-state-gate-off-logger+
-    +voice-state-gate-trigger+))
+    +voice-state-gate-retrigger+))
 
 (defun make-voice-state (name environment voice-number)
   (let ((voice-state (make-array (length +voice-states+))))
@@ -34,20 +34,25 @@
 	  (funcall (getf environment :register-event) name (format nil "GATE-~a-ON" voice-number)))
     (setf (elt voice-state +voice-state-gate-off-logger+)
 	  (funcall (getf environment :register-event) name (format nil "GATE-~a-OFF" voice-number)))
-    (setf (elt voice-state +voice-state-gate-trigger+) nil)
+    (setf (elt voice-state +voice-state-gate-retrigger+) nil)
     voice-state))
 
 (defun midi-interface (name environment &key
 					  (voice-count 1)
-					  (note-number-to-cv (lambda (note-number) (/ note-number 12))))
+					  (note-number-to-cv (lambda (note-number) (/ note-number 12)))
+					  (play-mode :PLAY-MODE-POLY))
+  "play-mode: :PLAY-MODE-POLY, :PLAY-MODE-UNISONO"
   (let* ((voice-states (make-array voice-count))
 	 (output-socket-lookup-table (make-hash-table :test #'eq))
-	 (voice-manager (make-instance 'cl-synthesizer-midi-voice-manager:voice-manager :voice-count voice-count))
+	 (voice-manager (make-instance 'cl-synthesizer-midi-voice-manager:voice-manager
+				       :voice-count (if (eq play-mode :PLAY-MODE-POLY) voice-count 1)))
 	 (outputs (concatenate 'list
 			       (cl-synthesizer-macro-util:make-keyword-list "CV" voice-count)
 			       (cl-synthesizer-macro-util:make-keyword-list "GATE" voice-count))))
     (dotimes (i voice-count)
-      (setf (elt voice-states i) (make-voice-state name environment i))
+      (if (or (eq 0 i) (not (eq play-mode :PLAY-MODE-UNISONO)))
+	  (setf (elt voice-states i) (make-voice-state name environment i))
+	  (setf (elt voice-states i) (elt voice-states 0)))
       (let ((cur-i i)) ;; new context for the lambdas
 	(setf (gethash (cl-synthesizer-macro-util:make-keyword "CV" cur-i) output-socket-lookup-table)
 	      (lambda () (elt (elt voice-states cur-i) +voice-state-cv+)))
@@ -63,7 +68,7 @@
 			 (error (format nil "Unknown input ~a requested from ~a" output name)))
 		     (funcall handler)))
      :update (lambda (&key (midi-event nil))
-	       (declare (optimize (debug 3) (speed 0) (space 0)))
+	       ;;(declare (optimize (debug 3) (speed 0) (space 0)))
 	       ;;(break)
 	       (if midi-event
 		   (cond
--- test/modules/test-midi-interface.lisp
+++ test/modules/test-midi-interface.lisp
@@ -1,11 +1,12 @@
 (in-package :cl-synthesizer-test)
 
-(defun test-midi-interface-make-midi-interface (voice-count)
+(defun test-midi-interface-make-midi-interface (voice-count &key (play-mode :PLAY-MODE-POLY))
   (cl-synthesizer-modules-midi-interface:midi-interface
    "Test-Midi-Interface"
    (cl-synthesizer::make-environment)
    :voice-count voice-count
-   :note-number-to-cv (lambda (n) (* 1000 n))))
+   :note-number-to-cv (lambda (n) (* 1000 n))
+   :play-mode play-mode))
 
 (defun test-midi-interface-get-output (ifc output)
   (funcall (getf ifc :get-output) output))
@@ -21,7 +22,7 @@ test-case '(:voice-count 2
 |#
 
 (defun run-test-case-midi-ifc (test-case)
-  (let ((ifc (test-midi-interface-make-midi-interface (getf test-case :voice-count))))
+  (let ((ifc (test-midi-interface-make-midi-interface (getf test-case :voice-count) :play-mode (getf test-case :play-mode))))
     (dolist (cur-test-case (getf test-case :test-cases))
       (test-midi-interface-update ifc (getf cur-test-case :event))
       (dolist (cur-output (getf cur-test-case :outputs))
@@ -29,14 +30,14 @@ test-case '(:voice-count 2
 
 (define-test test-midi-interface-1 ()
 	     (let ((test
-		    '(:voice-count 2
+		    '(:voice-count 2 :play-mode :PLAY-MODE-POLY
 		      :test-cases
 		      ((:event nil :outputs ((:CV-1 0) (:CV-2 0) (:GATE-1 0) (:GATE-2 0)))))))
 	       (run-test-case-midi-ifc test)))
 
 (define-test test-midi-interface-2 ()
 	     (let ((test
-		    `(:voice-count 2
+		    `(:voice-count 2 :play-mode :PLAY-MODE-POLY
 		      :test-cases
 		      ((:event ,(cl-synthesizer-midi-event:make-note-on-event 1 64 0)
 			       :outputs ((:CV-1 64000)
@@ -48,7 +49,7 @@ test-case '(:voice-count 2
 
 (define-test test-midi-interface-3 ()
 	     (let ((test
-		    `(:voice-count 2
+		    `(:voice-count 2 :play-mode :PLAY-MODE-POLY
 		      :test-cases
 		      (
 		       (:event ,(cl-synthesizer-midi-event:make-note-on-event 1 64 0)
@@ -68,7 +69,7 @@ test-case '(:voice-count 2
 
 (define-test test-midi-interface-4 ()
 	     (let ((test
-		    `(:voice-count 2
+		    `(:voice-count 2 :play-mode :PLAY-MODE-POLY
 		      :test-cases
 		      (
 		       (:event ,(cl-synthesizer-midi-event:make-note-on-event 1 32 0)
@@ -92,7 +93,7 @@ test-case '(:voice-count 2
 ;; die mittlere Note wegnehmen
 (define-test test-midi-interface-4 ()
 	     (let ((test
-		    `(:voice-count 2
+		    `(:voice-count 2 :play-mode :PLAY-MODE-POLY
 		      :test-cases
 		      (
 		       (:event ,(cl-synthesizer-midi-event:make-note-on-event 1 32 0)
@@ -120,3 +121,76 @@ test-case '(:voice-count 2
 		       ))))
 	       (run-test-case-midi-ifc test)))
 
+;;
+;; unisono tests
+;;
+(define-test test-midi-interface-unisono-1 ()
+	     (let ((test
+		    `(:voice-count 2 :play-mode :PLAY-MODE-UNISONO
+		      :test-cases
+		      (
+		       (:event ,(cl-synthesizer-midi-event:make-note-on-event 1 64 0)
+			       :outputs ((:CV-1 64000)
+					 (:GATE-1 5.0)
+					 (:CV-2 64000)
+					 (:GATE-2 5.0)))
+		       (:event ,(cl-synthesizer-midi-event:make-note-on-event 1 32 0)
+			       :outputs ((:CV-1 32000)
+					 (:GATE-1 5.0)
+					 (:CV-2 32000)
+					 (:GATE-2 5.0)))
+		       ))))
+	       (run-test-case-midi-ifc test)))
+
+(define-test test-midi-interface-unisono-2 ()
+	     (let ((test
+		    `(:voice-count 2 :play-mode :PLAY-MODE-UNISONO
+		      :test-cases
+		      (
+		       (:event ,(cl-synthesizer-midi-event:make-note-on-event 1 64 0)
+			       :outputs ((:CV-1 64000)
+					 (:GATE-1 5.0)
+					 (:CV-2 64000)
+					 (:GATE-2 5.0)))
+		       (:event ,(cl-synthesizer-midi-event:make-note-on-event 1 32 0)
+			       :outputs ((:CV-1 32000)
+					 (:GATE-1 5.0)
+					 (:CV-2 32000)
+					 (:GATE-2 5.0)))
+		       (:event ,(cl-synthesizer-midi-event:make-note-off-event 1 32 0)
+			       :outputs ((:CV-1 64000)
+					 (:GATE-1 5.0)
+					 (:CV-2 64000)
+					 (:GATE-2 5.0)))
+		       
+		       ))))
+	       (run-test-case-midi-ifc test)))
+
+(define-test test-midi-interface-unisono-3 ()
+	     (let ((test
+		    `(:voice-count 2 :play-mode :PLAY-MODE-UNISONO
+		      :test-cases
+		      (
+		       (:event ,(cl-synthesizer-midi-event:make-note-on-event 1 64 0)
+			       :outputs ((:CV-1 64000)
+					 (:GATE-1 5.0)
+					 (:CV-2 64000)
+					 (:GATE-2 5.0)))
+		       (:event ,(cl-synthesizer-midi-event:make-note-on-event 1 32 0)
+			       :outputs ((:CV-1 32000)
+					 (:GATE-1 5.0)
+					 (:CV-2 32000)
+					 (:GATE-2 5.0)))
+		       (:event ,(cl-synthesizer-midi-event:make-note-off-event 1 64 0)
+			       :outputs ((:CV-1 32000)
+					 (:GATE-1 5.0)
+					 (:CV-2 32000)
+					 (:GATE-2 5.0)))
+		       (:event ,(cl-synthesizer-midi-event:make-note-off-event 1 32 0)
+			       :outputs ((:CV-1 32000)
+					 (:GATE-1 0)
+					 (:CV-2 32000)
+					 (:GATE-2 0)))
+		       
+		       ))))
+	       (run-test-case-midi-ifc test)))
