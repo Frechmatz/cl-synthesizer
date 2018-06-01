94880639c8ffda3c45ad153c2496247b6071abd3
Oliver Voeltz <frechmatz@gmx.de>
2018-06-01 19:43:05 +0200

    Use monitor instead of wave-writer module

--- playground/adsr.lisp
+++ playground/adsr.lisp
@@ -34,40 +34,28 @@
 				:release-ms 1000)
     (cl-synthesizer::add-module rack "VCA" #'cl-synthesizer-modules-vca:vca)
 
-    ;; Attach modules to the outputs of the MIDI Interface
     (cl-synthesizer::add-patch rack "MIDI-IFC" :cv-1 "VCO" :cv)
     (cl-synthesizer::add-patch rack "MIDI-IFC" :gate-1 "ADSR" :gate)
-
-    ;; Set up the Wave-File-Recorder
-    ;; Writes the ADSR-Output and the VCA-Output into a 2 channel wave file
-    (cl-synthesizer::add-module
-     rack "WAVE-WRITER"
-     #'cl-synthesizer-modules-wave-file-writer::two-channel-wave-file-writer
-     :filename "/Users/olli/waves/adsrplayground.wav")
-    
-    ;; Attach output of VCO to VCA
     (cl-synthesizer::add-patch rack "VCO" :sine "VCA" :input)
+    (cl-synthesizer::add-patch rack "VCA" :out "LINE-OUT" :channel-1)
+    (cl-synthesizer::add-patch rack "ADSR" :cv "VCA" :cv)
 
-    ;; Attach output of VCA to LINE-OUT and Recorder
-    (cl-synthesizer::add-module rack "MULTIPLE-VCA-OUT" #'cl-synthesizer-modules-multiple:multiple-4)
-    (cl-synthesizer::add-patch rack "VCA" :out "MULTIPLE-VCA-OUT" :input)
-    (cl-synthesizer::add-patch rack "MULTIPLE-VCA-OUT" :out-1 "LINE-OUT" :channel-1)
-    (cl-synthesizer::add-patch rack "MULTIPLE-VCA-OUT" :out-2 "WAVE-WRITER" :channel-1)
-
-    ;; Attach output of ADSR to VCA and Recorder
-    (cl-synthesizer::add-module rack "MULTIPLE-ADSR-OUT" #'cl-synthesizer-modules-multiple:multiple-4)
-    (cl-synthesizer::add-patch rack "ADSR" :cv "MULTIPLE-ADSR-OUT" :input)
-    (cl-synthesizer::add-patch rack "MULTIPLE-ADSR-OUT" :out-1 "VCA" :cv)
-    (cl-synthesizer::add-patch rack "MULTIPLE-ADSR-OUT" :out-2 "WAVE-WRITER" :channel-2)
-
-    ;; Add VCA output monitor
+    ;; Add ADSR-Output and VCA-Output monitor
     (cl-synthesizer:register-monitor
      rack
-     "My Monitor"
+     "ADSR-VCA"
      #'cl-synthesizer-monitor:wave-file-handler
-     '((:channel-1 "MULTIPLE-VCA-OUT" :output-socket :out-1))
-     :filename "/Users/olli/waves/monitor.wav")
-  
+     '((:channel-1 "VCA" :output-socket :out) (:channel-2 "ADSR" :output-socket :cv))
+     :filename "/Users/olli/waves/adsrplayground.wav")
+
+    ;; Add LINE-OUT Monitor
+    (cl-synthesizer:register-monitor
+     rack
+     "LINE-OUT"
+     #'cl-synthesizer-monitor:wave-file-handler
+     '((:channel-1 "LINE-OUT" :input-socket :channel-1))
+     :filename "/Users/olli/waves/lineout.wav")
+    
     rack))
 
 (defun play ()
