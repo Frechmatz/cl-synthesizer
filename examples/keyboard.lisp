280778790dcea2c0b7249cb58f87da62d21f3a51
Oliver Voeltz <frechmatz@gmx.de>
2018-06-01 20:14:41 +0200

    Wave-File writing via monitor

--- examples/keyboard.lisp
+++ examples/keyboard.lisp
@@ -17,6 +17,7 @@
      :voice-count 2)
     (cl-synthesizer::add-patch rack "MIDI-IN" :midi-events "MIDI-IFC" :midi-events)
     (flet ((add-voice (prefix voice-number)
+	     (declare (optimize (debug 3) (speed 0) (space 0)))
 	     (flet ((make-module-name (name)
 		       (format nil "~a-~a-~a" prefix voice-number name)))
 	       (cl-synthesizer::add-module
@@ -40,28 +41,26 @@
 		rack
 		"MIDI-IFC"
 		(cl-synthesizer-macro-util:make-keyword "GATE" voice-number)
-		(make-module-name "ADSR") :gate)
-	       (values (make-module-name "VCA") :out))))
-      (cl-synthesizer::add-module
-       rack "WAVE-WRITER"
-       #'cl-synthesizer-modules-wave-file-writer::two-channel-wave-file-writer
+		(make-module-name "ADSR") :gate))))
+
+      ;;
+      ;; TODO: Numbering is confusing. When will number be incremented by one and when not?
+      ;;
+      (add-voice "VOICE" 0)
+      (cl-synthesizer::add-patch rack "VOICE-0-VCA" :out "LINE-OUT" :channel-1)
+      (add-voice "VOICE" 1)
+      (cl-synthesizer::add-patch rack "VOICE-1-VCA" :out "LINE-OUT" :channel-2)
+      
+      ;; Add LINE-OUT Monitor
+      (cl-synthesizer:register-monitor
+       rack
+       "LINE-OUT"
+       #'cl-synthesizer-monitor:wave-file-handler
+       '((:channel-1 "LINE-OUT" :input-socket :channel-1) (:channel-2 "LINE-OUT" :input-socket :channel-2))
        :filename "/Users/olli/waves/lineout.wav")
-      (multiple-value-bind (output-module-name output-module-socket)
-	  (add-voice "VOICE" 0)
-	  ;; Connect voice to wave writer and line out
-	  (cl-synthesizer::add-module rack "MULTIPLE-1" #'cl-synthesizer-modules-multiple:multiple-4)
-	  (cl-synthesizer::add-patch rack output-module-name output-module-socket "MULTIPLE-1" :input)
-	  (cl-synthesizer::add-patch rack "MULTIPLE-1" :out-1 "LINE-OUT" :channel-1)
-	  (cl-synthesizer::add-patch rack "MULTIPLE-1" :out-2 "WAVE-WRITER" :channel-1))
-      (multiple-value-bind (output-module-name output-module-socket)
-	  (add-voice "VOICE" 1)
-	;; Connect voice to wave writer and line out
-	(cl-synthesizer::add-module rack "MULTIPLE-2" #'cl-synthesizer-modules-multiple:multiple-4)
-	(cl-synthesizer::add-patch rack output-module-name output-module-socket "MULTIPLE-2" :input)
-	(cl-synthesizer::add-patch rack "MULTIPLE-2" :out-1 "LINE-OUT" :channel-2)
-	(cl-synthesizer::add-patch rack "MULTIPLE-2" :out-2 "WAVE-WRITER" :channel-2))
-	rack)))
-    
+      
+      rack)))
+
 ;;(cl-synthesizer-util:play-rack (cl-synthesizer-examples::synthesizer-example-keyboard) 20 :attach-speaker t :attach-midi t)
 
 
