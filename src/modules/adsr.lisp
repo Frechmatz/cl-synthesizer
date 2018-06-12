00d4b8a808dc434b12f1995b2779b340e05456e4
Oliver Voeltz <frechmatz@gmx.de>
2018-06-12 21:01:31 +0200

    Moved has-segment-completed from module to segment macros

--- src/modules/adsr.lisp
+++ src/modules/adsr.lisp
@@ -53,73 +53,73 @@ The function returns the current segment index or nil."
     
 
 ;;
-;; Segment macros (to be used within the ADSR function definition)
+;; Segment macros (to be used within the ADSR module function definition)
 ;;
 
-(defmacro hold ((&key segment-name requires-gate))
+(defmacro hold ((&key segment-name))
   "Generates a segment whose result remains at cur-cv until the gate drops to 0."
-  `(list
-    :init (lambda ()
-	    (format t "~%Initializing segment ~a~%" ,segment-name))
-    :update (lambda()
-	      (if (has-segment-completed nil nil ,requires-gate)
-		  :CONTINUE
-		  :DONE))))
+  `(flet ((has-segment-completed ()
+	    (not is-gate)))
+     (list
+      :init (lambda ()
+	      (format t "~%Initializing segment ~a~%" ,segment-name))
+      :update (lambda()
+		(if (has-segment-completed)
+		    :CONTINUE
+		    :DONE)))))
 
-(defmacro slope ((&key segment-name requires-gate target-cv time-ms))
+(defmacro ramp ((&key segment-name requires-gate target-cv time-ms))
   "Generates a segment whose output starts at cur-cv and descends/ascends to the given target
 voltage within the given time interval. Depending on the 'requires-gate' parameter the segment
 terminates when the gate drops to 0."
   `(let ((total-ticks nil) (elapsed-ticks nil) (transfer-fn nil))
-    (list
-     :init (lambda ()
-	     (format t "~%Initializing segment ~a~%" ,segment-name)
-	     (setf total-ticks (* ticks-per-ms ,time-ms))
-	     (setf elapsed-ticks -1)
-	     (setf transfer-fn (cl-synthesizer-core:linear-converter
-				:input-min 0
-				:input-max total-ticks
-				:output-min cur-cv
-				:output-max ,target-cv)))
-     :update (lambda()
-	       (setf elapsed-ticks (+ 1 elapsed-ticks))
-	       (if (has-segment-completed elapsed-ticks total-ticks ,requires-gate)
-		   :CONTINUE
-		   (progn
-		     (setf cur-cv (funcall (getf transfer-fn :get-y) elapsed-ticks))
-		     :DONE))))))
+     (flet ((has-segment-completed (elapsed-ticks total-ticks requires-gate)
+	      (if (and requires-gate (not is-gate))
+		  t
+		  (and elapsed-ticks total-ticks (>= elapsed-ticks total-ticks)))))
+       (list
+	:init (lambda ()
+		(format t "~%Initializing segment ~a~%" ,segment-name)
+		(setf total-ticks (* ticks-per-ms ,time-ms))
+		(setf elapsed-ticks -1)
+		(setf transfer-fn (cl-synthesizer-core:linear-converter
+				   :input-min 0
+				   :input-max total-ticks
+				   :output-min cur-cv
+				   :output-max ,target-cv)))
+	:update (lambda()
+		  (setf elapsed-ticks (+ 1 elapsed-ticks))
+		  (if (has-segment-completed elapsed-ticks total-ticks ,requires-gate)
+		      :CONTINUE
+		      (progn
+			(setf cur-cv (funcall (getf transfer-fn :get-y) elapsed-ticks))
+			:DONE)))))))
   
 (defun adsr (name environment &key (attack-ms 1000) (attack-cv 5) (decay-ms 1000) (decay-cv 3) (release-ms 1000))  
   (declare (ignore name))
-  (declare (optimize (debug 3) (speed 0) (space 0)))
-  (let* ((sample-rate (getf environment :sample-rate))
-	 (is-gate nil)
+  ;;(declare (optimize (debug 3) (speed 0) (space 0)))
+  (let* ((is-gate nil)
 	 (cur-cv 0)
-	 ;; error is about 2 promille at sample frequency of 44100 
-	 (ticks-per-ms (floor (/ sample-rate 1000))))
-    (labels ((has-segment-completed (elapsed-ticks total-ticks requires-gate)
-	       (declare (optimize (debug 3) (speed 0) (space 0)))
-	       (if (and requires-gate (not is-gate))
-		   t
-		   (and elapsed-ticks total-ticks (>= elapsed-ticks total-ticks)))))
-      (let ((controller (segments-controller
-		   (list
-		    (slope (:segment-name "Attack" :requires-gate t :target-cv attack-cv :time-ms attack-ms))
-		    (slope (:segment-name "Decay" :requires-gate t :target-cv decay-cv :time-ms decay-ms))
-		    (hold (:segment-name "Sustain" :requires-gate t))
-		    (slope (:segment-name "Release" :requires-gate nil :target-cv 0 :time-ms release-ms))))))
-	(list
-	 :inputs (lambda () '(:gate))
-	 :outputs (lambda () '(:cv))
-	 :get-output (lambda (output)
-		       (declare (ignore output))
-		       cur-cv)
-	 :update (lambda (&key (gate 0))
-		   (declare (optimize (debug 3) (speed 0) (space 0)))
-		   (let ((previous-gate is-gate) (restart nil))
-		     (setf is-gate (if (>= gate 4.9) t nil))
-		     (setf restart (and is-gate (not previous-gate)))
-		     (if restart
-			 (setf cur-cv 0))
-		     (funcall controller restart))))))))
+	 (ticks-per-ms (floor (/ (getf environment :sample-rate) 1000))))
+    (let ((controller
+	   (segments-controller
+	    (list
+	     (ramp (:segment-name "Attack" :requires-gate t :target-cv attack-cv :time-ms attack-ms))
+	     (ramp (:segment-name "Decay" :requires-gate t :target-cv decay-cv :time-ms decay-ms))
+	     (hold (:segment-name "Sustain"))
+	     (ramp (:segment-name "Release" :requires-gate nil :target-cv 0 :time-ms release-ms))))))
+      (list
+       :inputs (lambda () '(:gate))
+       :outputs (lambda () '(:cv))
+       :get-output (lambda (output)
+		     (declare (ignore output))
+		     cur-cv)
+       :update (lambda (&key (gate 0))
+		 ;;(declare (optimize (debug 3) (speed 0) (space 0)))
+		 (let ((previous-gate is-gate) (restart nil))
+		   (setf is-gate (if (>= gate 4.9) t nil))
+		   (setf restart (and is-gate (not previous-gate)))
+		   (if restart
+		       (setf cur-cv 0))
+		   (funcall controller restart)))))))
 
