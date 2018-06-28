57c2889a5f82021d5e209f6f928c8c07dfd39005
Oliver Voeltz <frechmatz@gmx.de>
2018-06-28 19:49:34 +0200

    Unit tests

--- test/modules/envelope/test-envelope.lisp
+++ test/modules/envelope/test-envelope.lisp
@@ -32,8 +32,8 @@ test-case:
 	      (expected-cv (getf cur-test-case :expected-cv)))
 	  (dotimes (i ticks)
 	    (funcall (getf module :update) :gate gate))
-	  ;; http://www.lispworks.com/documentation/HyperSpec/Body/f_equalp.htm
-	  (assert-equality #'= expected-cv (funcall (getf module :get-output) :cv)))))))
+	  ;; https://stackoverflow.com/questions/547436/whats-the-difference-between-eq-eql-equal-and-equalp-in-common-lisp
+	  (assert-equality #'= expected-cv (float (funcall (getf module :get-output) :cv))))))))
 
 (define-test test-envelope-1 ()
 	     (let ((test
@@ -67,4 +67,26 @@ test-case:
 				 (:time-ticks nil :target-cv 20 :required-gate-state :off))
 		      :test-cases ((:gate :on :ticks 1 :expected-cv 5)
 				   (:gate :off :ticks 1 :expected-cv 20)))))
+	       (run-test-case-envelope test)))
+
+(define-test test-envelope-adsr ()
+	     (let ((test
+		    '(:segments ((:time-ticks 1000 :target-cv 100 :required-gate-state :on)
+				 (:time-ticks 1000 :target-cv 50 :required-gate-state :on)
+				 (:required-gate-state :on)
+				 (:time-ticks 1000 :target-cv 0 :required-gate-state :off))
+		      :test-cases ((:gate :off :ticks 20 :expected-cv 0)
+				   ;; attack
+				   (:gate :on :ticks 1 :expected-cv 0.1)
+				   (:gate :on :ticks 999 :expected-cv 100)
+				   ;; decay
+				   (:gate :on :ticks 1 :expected-cv 99.95)
+				   (:gate :on :ticks 999 :expected-cv 50)
+				   ;; sustain
+				   (:gate :on :ticks 1 :expected-cv 50)
+				   (:gate :on :ticks 1000 :expected-cv 50)
+				   ;; release
+				   (:gate :off :ticks 1 :expected-cv 49.95)
+				   (:gate :off :ticks 999 :expected-cv 0)
+				   ))))
 	       (run-test-case-envelope test)))
