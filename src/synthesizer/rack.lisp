1dfe5812ab1c133a5c03a62660be10e33a044df5
Oliver Voeltz <frechmatz@gmx.de>
2018-07-20 22:37:01 +0200

    Rearranged function definitions to make dependencies more obvious

--- src/synthesizer/rack.lisp
+++ src/synthesizer/rack.lisp
@@ -54,30 +54,6 @@
 ;;
 
 ;;
-;; Patch
-;;
-
-(defclass rack-module-patch ()
-  ((rack-module :initarg nil)
-   (socket :initarg nil))
-  (:documentation ""))
-
-(defun get-rack-patch-target-name (patch)
-  (get-rack-module-name (slot-value patch 'rack-module)))
-
-(defun get-rack-patch-socket (patch)
-  (slot-value patch 'socket))
-
-(defun get-rack-patch-module (patch)
-  (slot-value patch 'rack-module))
-
-(defun make-rack-module-patch (rm socket)
-  (let ((c (make-instance 'rack-module-patch)))
-    (setf (slot-value c 'rack-module) rm)
-    (setf (slot-value c 'socket) socket)
-    c))
-
-;;
 ;; Rack-Module
 ;; 
 
@@ -132,6 +108,30 @@
 
 (defun get-rack-module-output-patch (rm output-socket)
   (gethash output-socket (slot-value rm 'output-patches)))
+
+;;
+;; Patch
+;;
+
+(defclass rack-module-patch ()
+  ((rack-module :initarg nil)
+   (socket :initarg nil))
+  (:documentation ""))
+
+(defun get-rack-patch-target-name (patch)
+  (get-rack-module-name (slot-value patch 'rack-module)))
+
+(defun get-rack-patch-socket (patch)
+  (slot-value patch 'socket))
+
+(defun get-rack-patch-module (patch)
+  (slot-value patch 'rack-module))
+
+(defun make-rack-module-patch (rm socket)
+  (let ((c (make-instance 'rack-module-patch)))
+    (setf (slot-value c 'rack-module) rm)
+    (setf (slot-value c 'socket) socket)
+    c))
 
 ;;
 ;; Rack
