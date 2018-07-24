e7f87e9d201f63a3ebc3b01a0997d1f5f75bc22a
Oliver Voeltz <frechmatz@gmx.de>
2018-07-24 18:16:40 +0200

    re-arranged order of exports

--- src/synthesizer/packages.lisp
+++ src/synthesizer/packages.lisp
@@ -6,16 +6,15 @@
    :invalid-arguments-error
    :signal-invalid-arguments-error
    :make-environment
+   :get-environment
    :make-rack
-   :get-line-out-adapter
-   :get-midi-in-adapter
    :add-module
    :add-patch
-   :update
-   :shutdown
    :add-hook
-   :get-environment
    :get-module
    :get-patch
-   ))
+   :update
+   :shutdown
+   :get-line-out-adapter
+   :get-midi-in-adapter))
 
