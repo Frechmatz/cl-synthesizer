738bcd74f866d9af7e15ada6d3b27324a14052aa
Oliver Voeltz <frechmatz@gmx.de>
2018-07-26 20:26:53 +0200

    Removed ADSR module

--- playground/exponential-vca.lisp
+++ playground/exponential-vca.lisp
@@ -9,60 +9,39 @@
 
 (in-package :cl-synthesizer-playground-exp-vca)
 
-(defparameter *exp-vca-adsr-environment* (cl-synthesizer:make-environment))
-
 (defun synthesizer-playground-exponential-vca ()
   "Exponential VCA example"
-  (let ((rack (cl-synthesizer:make-rack :environment *exp-vca-adsr-environment*)))
-
-    ;; Set up MIDI Interface and connect it to the MIDI input of the Rack
-    (cl-synthesizer:add-module rack "MIDI-IFC"
-				#'cl-synthesizer-modules-midi-interface:midi-interface :voice-count 1)
-    (cl-synthesizer:add-patch rack "MIDI-IN" :midi-events "MIDI-IFC" :midi-events)
-
-    ;; Setup ADSR
-    (cl-synthesizer:add-module rack "ADSR" #'cl-synthesizer-modules-envelope:envelope
-			       :segments '(;; Attack
-					   (:duration-ms 1000 :target-cv 5 :required-gate-state :on)
-					   ;; Decay
-					   (:duration-ms 500 :target-cv 2.5 :required-gate-state :on)
-					   ;; Sustain
-					   (:required-gate-state :on)
-					   ;; Release
-					   (:duration-ms 1000 :target-cv 0 :required-gate-state :off)))
-    ;; Connect ADSR Gate input with Gate output of MIDI-IFC
-    (cl-synthesizer:add-patch rack "MIDI-IFC" :gate-1 "ADSR" :gate)
-    ;; Dispatch output of ADSR to a multiple
-    (cl-synthesizer:add-module rack "ADSR-CV-MULTIPLE" #'cl-synthesizer-modules-multiple:multiple
-			       :output-count 2)
-    (cl-synthesizer:add-patch rack "ADSR" :cv "ADSR-CV-MULTIPLE" :input)
-
-    ;; Set up VCA which will follow the CV of ADSR with linear characteristic
-    ;; this is realized with a fixed input voltage of the CV input of the VCA
-    (cl-synthesizer:add-module rack "VCA-LINEAR" #'cl-synthesizer-modules-vca::vca-ng
-			       :max-amplification 1.0
-			       :max-amplification-cv 2.5)
-    (cl-synthesizer:add-module rack "VCA-LINEAR-CONST-CV" #'cl-synthesizer-modules-fixed-output:fixed-output :value 2.5)
-    (cl-synthesizer:add-patch rack "VCA-LINEAR-CONST-CV" :out "VCA-LINEAR" :cv)
-    (cl-synthesizer:add-patch rack "ADSR-CV-MULTIPLE" :output-1 "VCA-LINEAR" :input)
-
-    ;; Set up VCA which will follow the cv of ADSR with exponential characteristic
-    ;; this is realized with a fixed input voltage of the VCA and the CV input
-    ;; of the VCA being modulated by the CV of the ADSR
-    (cl-synthesizer:add-module rack "VCA-EXPONENTIAL" #'cl-synthesizer-modules-vca::vca-ng
-			       :max-amplification 1.0
-			       :max-amplification-cv 5.0) ;; max output of ADSR CV
-    (cl-synthesizer:add-module rack "VCA-EXPONENTIAL-CONST-INPUT" #'cl-synthesizer-modules-fixed-output:fixed-output :value 5.0)
-    (cl-synthesizer:add-patch rack "VCA-EXPONENTIAL-CONST-INPUT" :out "VCA-EXPONENTIAL" :input)
-    (cl-synthesizer:add-patch rack "ADSR-CV-MULTIPLE" :output-2 "VCA-EXPONENTIAL" :cv)
+  (let ((rack (cl-synthesizer:make-rack :environment (cl-synthesizer:make-environment))))
+    
+    (cl-synthesizer:add-module
+     rack "LFO"
+     #'cl-synthesizer-modules-vco:vco
+     :base-frequency 1.0
+     :v-peak 5)
+    
+    (cl-synthesizer:add-module
+     rack "VCA"
+     #'cl-synthesizer-modules-vca::vca-ng
+     :max-amplification 5.0
+     :max-amplification-cv 5.0)
+    
+    ;; LFO modulates amplification of VCA
+    (cl-synthesizer:add-patch rack "LFO" :triangle "VCA" :cv)
+    ;; Feed a constant voltage into the VCA :input socket
+    (cl-synthesizer:add-module rack "CONST-VOLTAGE" #'cl-synthesizer-modules-fixed-output:fixed-output :value 1.0)
+    (cl-synthesizer:add-patch rack "CONST-VOLTAGE" :out "VCA" :input)
 
     ;; Add monitor
+    ;; expected output:
+    ;; both outputs of VCA should reach the maximum volatge of 5.0
+    ;; but with different curve progressions
     (cl-synthesizer-monitor:add-monitor
      rack
      #'cl-synthesizer-monitor-wave-handler:wave-file-handler
-     '((:channel-1 "ADSR" :output-socket :cv)
-       (:channel-2 "VCA-LINEAR" :output-socket :output-linear)
-       (:channel-3 "VCA-EXPONENTIAL" :output-socket :output-exponential)
+     '((:channel-1 "LFO" :output-socket :triangle)
+       (:channel-2 "VCA" :input-socket :input)
+       (:channel-3 "VCA" :output-socket :output-linear)
+       (:channel-4 "VCA" :output-socket :output-exponential)
        )
      :filename "/Users/olli/waves/vcaplayground.wav")
 
@@ -71,21 +50,6 @@
 (defun play ()
   (cl-synthesizer-util:play-rack
    (synthesizer-playground-exponential-vca)
-   5
-   ;;:attach-speaker t
-   :midi-device
-   (cl-synthesizer-device-midi-sequencer:midi-sequencer
-    "Midi-Device"
-    *exp-vca-adsr-environment*
-    :events
-    (list 
-     (list :timestamp-milli-seconds 20
-	   :midi-events (list
-			 (cl-synthesizer-midi-event:make-note-on-event 1 69 100)))
-     (list :timestamp-milli-seconds 2020
-	   :midi-events (list
-			 (cl-synthesizer-midi-event:make-note-off-event 1 69 100)))
-
-     ))))
+   5))
 
 ;; (play)
