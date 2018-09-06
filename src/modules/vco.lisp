51372ea644806a3fd7a17da335c62c337af41c01
Oliver Voeltz <frechmatz@gmx.de>
2018-09-06 21:57:44 +0200

    Added doc strings

--- src/modules/vco.lisp
+++ src/modules/vco.lisp
@@ -7,7 +7,30 @@
 (in-package :cl-synthesizer-modules-vco)
 
 (defun vco-base (name environment transfer-function &key f-max v-peak (duty-cycle 0.5))
-  ""
+  "Creates a Voltage Controlled Oscillator module. The function has the following arguments:
+    <ul>
+	<li>name Name of the module.</li>
+	<li>environment The synthesizer environment.</li>
+	<li>transfer-function A function that converts the frequency control voltage into
+	    a frequency. This function is called with the current frequency control voltage and
+	    must return a frequency. Frequencies greater than f-max will be clipped. Negative
+	    frequencies will be clipped to 0Hz.</li>
+	<li>:f-max The maximum frequency of the oscillator. f-max must be greater than 0.</li>
+	<li>:v-peak Absolute value of the maximal voltage (positive/negative) emitted by the oscillator.</li>
+	<li>:duty-cycle The duty cycle of the square wave. 0 >= duty-cycle <= 1. Default value is 0.5.</li>
+    </ul>
+    The module has the following inputs:
+    <ul>
+	<li>:cv Frequency control voltage.</li>
+    </ul>
+    The module has the following outputs:
+    <ul>
+	<li>:sine A sine wave.</li>
+	<li>:triangle A triangle wave.</li>
+	<li>:saw A saw wave.</li>
+	<li>:square A square wave.</li>
+    </ul>
+    See also modules vco-linear and vco-exponential."
   (if (not f-max)
       (cl-synthesizer:signal-assembly-error
        :format-control "f-max of VCO ~a must not be nil"
@@ -71,6 +94,24 @@
 						      phi :duty-cycle duty-cycle)))))))))
 
 (defun vco-exponential (name environment &key base-frequency f-max v-peak (duty-cycle 0.5))
+  "Creates a Voltage Controlled Oscillator module with 1V/Octave characteristic.
+   The function has the following arguments:
+    <ul>
+	<li>name Name of the module.</li>
+	<li>environment The synthesizer environment.</li>
+	<li>:base-frequency The frequency emitted by the oscillator at a frequency control voltage of 0.</li>
+	<li>:f-max See vco-base.</li>
+	<li>:v-peak See vco-base.</li>
+	<li>:duty-cycle See vco-base.</li>
+    </ul>
+    The module has the following inputs:
+    <ul>
+	<li>:cv Frequency control voltage. For a given base-frequency of 440Hz a
+	    control voltage of 1.0 results in a frequency of 880Hz and a control
+	    voltage of -1.0 results in a frequency of 220Hz.
+	</li>
+    </ul>
+    For the outputs of the module see vco-base."
   (if (not base-frequency)
       (cl-synthesizer:signal-assembly-error
        :format-control "base-frequency of VCO ~a must not be nil"
@@ -89,6 +130,23 @@
    :duty-cycle duty-cycle))
 
 (defun vco-linear (name environment &key base-frequency f-max v-peak cv-max (duty-cycle 0.5))
+  "Creates a Voltage Controlled Oscillator module with linear characteristic.
+   The function has the following arguments:
+    <ul>
+	<li>name Name of the module.</li>
+	<li>environment The synthesizer environment.</li>
+	<li>:cv-max The frequency control voltage which represents the maximum frequency of the oscillator.</li>
+	<li>:base-frequency The frequency emitted by the oscillator at a frequency control voltage of 0.</li>
+	<li>:f-max See vco-base.</li>
+	<li>:v-peak See vco-base.</li>
+	<li>:duty-cycle See vco-base.</li>
+    </ul>
+    The module has the following inputs:
+    <ul>
+	<li>:cv Frequency control voltage. For frequency calculation the absolute value
+	of the control voltage is used. The control voltage is clipped at :cv-max.</li>
+    </ul>
+    For the outputs of the module see vco-base."
   (if (not base-frequency)
       (cl-synthesizer:signal-assembly-error
        :format-control "base-frequency of VCO ~a must not be nil"
