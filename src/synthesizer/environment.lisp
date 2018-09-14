38015948604487de9eb3d80eee3e4b2a8688fcb0
Oliver Voeltz <frechmatz@gmx.de>
2018-09-14 19:42:28 +0200

    Reworked instantiation of MIDI/Audio devices

--- src/synthesizer/environment.lisp
+++ src/synthesizer/environment.lisp
@@ -3,5 +3,12 @@
 (defun make-environment (&key (sample-rate 44100))
     (list
      :sample-rate sample-rate
-     :output-directory "/Users/olli/"))
-
+     :output-directory "/Users/olli/"
+     :audio-device (list
+		    :symbol-name "SPEAKER-CL-OUT123"
+		    :package-name "CL-SYNTHESIZER-DEVICE-SPEAKER"
+		    :init-args (list :channel-count 2 :driver "coreaudio"))
+     :midi-device (list
+		   :symbol-name "MIDI-DEVICE"
+		   :package-name "CL-SYNTHESIZER-DEVICE-MIDI"
+		   :init-args nil)))
--- util/play-rack.lisp
+++ util/play-rack.lisp
@@ -1,22 +1,46 @@
 (in-package :cl-synthesizer-util)
 
+(defun instantiate-device (device-settings name environment)
+  (let ((symbol-name (getf device-settings :symbol-name))
+	(package-name (getf device-settings :package-name)))
+    (if (not symbol-name)
+	(error "Device instantiation failed. :symbol-name is a mandatory device configuration setting")) 
+    (if (not package-name)
+	(error "Device instantiation failed. :package-name is a mandatory device configuration setting")) 
+    (format t "~%Instantiating device ~a::~a..." symbol-name package-name)
+    (let ((ctor (find-symbol symbol-name package-name)))
+      (if ctor
+	  (apply ctor name environment (getf device-settings :init-args))
+	  (error (format nil "Device instantiation failed. Symbol ~a::~a not found" symbol-name package-name))))))
+
+(defun make-audio-device (name environment)
+  (let ((settings (getf environment :audio-device)))
+    (if settings
+	(instantiate-device
+	 settings
+	 name
+	 environment)
+	(error (format nil "Audio device required but not configured by environment")))))
+
+(defun make-midi-device (name environment)
+  (let ((settings (getf environment :midi-device)))
+    (if settings
+	(instantiate-device
+	 settings
+	 name
+	 environment)
+	(error (format nil "MIDI device required but not configured by environment")))))
+
 (defun play-rack (rack duration-seconds &key (attach-speaker nil) (attach-midi nil))
-  (let* ((start (get-internal-real-time))
-	 (environment (slot-value rack 'cl-synthesizer::environment))
-	 (sample-rate (getf environment :sample-rate))
-	 (ticks-to-play (* duration-seconds sample-rate)))
-    (format t "~%Ticks to play: ~a~%" ticks-to-play)
+  (let* ((environment (slot-value rack 'cl-synthesizer::environment)))
     (if attach-speaker
 	(funcall (getf (cl-synthesizer:get-line-out-adapter rack) :set-device)
-		 (cl-synthesizer-device-speaker:speaker-cl-out123 "SPEAKER" environment :channel-count 2 :driver "coreaudio")))
+		 (make-audio-device "SPEAKER" environment)))
     (if attach-midi
 	(funcall (getf (cl-synthesizer:get-midi-in-adapter rack) :set-device)
-		 (cl-synthesizer-device-midi:midi-device "MIDI" environment)))
-    (dotimes (i ticks-to-play)
+		 (make-midi-device "MIDI" environment)))
+    (dotimes (i (* duration-seconds (getf environment :sample-rate)))
       (cl-synthesizer:update rack))
     (cl-synthesizer:shutdown rack)
-    (let ((end (get-internal-real-time)))
-      (format t "~%Elapsed time in seconds after shutdown: ~a~%"
-	      (/ (- end start) internal-time-units-per-second))))
-  "DONE")
+    "DONE"))
 
