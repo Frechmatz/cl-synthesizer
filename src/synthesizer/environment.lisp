108ed80c5caff447aabf7fcdbe4706ac30962326
Oliver Voeltz <frechmatz@gmx.de>
2018-09-18 19:47:15 +0200

    Audio-Device: Pick channel count from environment instead of using hard coded value of 2

--- src/synthesizer/environment.lisp
+++ src/synthesizer/environment.lisp
@@ -1,13 +1,16 @@
 (in-package :cl-synthesizer)
 
-(defun make-environment (&key (sample-rate 44100))
+(defun make-environment (&key (sample-rate 44100) (channel-count 2))
     (list
      :sample-rate sample-rate
+     :channel-count channel-count
      :output-directory "/Users/olli/"
      :audio-device (list
 		    :symbol-name "SPEAKER-CL-OUT123"
 		    :package-name "CL-SYNTHESIZER-DEVICE-SPEAKER"
-		    :init-args (list :channel-count 2 :driver "coreaudio"))
+		    :init-args (list
+				:channel-count (lambda (environment) (getf environment :channel-count))
+				:driver "coreaudio"))
      :midi-device (list
 		   :symbol-name "MIDI-DEVICE"
 		   :package-name "CL-SYNTHESIZER-DEVICE-MIDI"
