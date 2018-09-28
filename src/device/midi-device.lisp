3650cff1d58588be62a268d76909917ae27d17d9
Oliver Voeltz <frechmatz@gmx.de>
2018-09-28 17:14:37 +0200

    Clean up

--- src/device/midi-device.lisp
+++ src/device/midi-device.lisp
@@ -2,7 +2,6 @@
 ;;
 ;; A MIDI Device based on CoreMidi 
 ;;
-;; Work in progress
 ;;
 
 (in-package :cl-synthesizer-device-midi)
@@ -10,29 +9,28 @@
 (defparameter *source* 1) ;; 0 == VMPK
 
 (defun midi-device (name environment)
-  (declare (ignore name))
-  (declare (ignore environment))
+  (declare (ignore name environment))
   (let ((event-queue (queues:make-queue :simple-cqueue)))
     (midi:initialize)
     (midi:set-midi-callback
      (midi:get-source *source*)
      :cc ;; for now listen to all control change events
      (lambda (chan control value)
-       (format t "[CC] Channel: ~d  Control: ~d  Value: ~d~%" chan control value)       
+       ;;(format t "[CC] Channel: ~d  Control: ~d  Value: ~d~%" chan control value)
        (queues:qpush event-queue (cl-synthesizer-midi-event:make-control-change-event chan control value))))
     (midi:set-midi-callback
      (midi:get-source *source*)
      :note-on
      (lambda (chan note vel)
        ;; [NOTE-ON] Channel: 1  Notenum: 61  Velocity: 15
-       (format t "[NOTE-ON ] Channel: ~d  Notenum: ~d  Velocity: ~d~%" chan note vel)
+       ;;(format t "[NOTE-ON ] Channel: ~d  Notenum: ~d  Velocity: ~d~%" chan note vel)
        (queues:qpush event-queue (cl-synthesizer-midi-event:make-note-on-event chan note vel))))
     (midi:set-midi-callback
      (midi:get-source *source*)
      :note-off
      (lambda (chan note vel)
        ;; [NOTE-OFF] Channel: 1  Notenum: 61  Velocity: 0
-       (format t "[NOTE-OFF] Channel: ~d  Notenum: ~d  Velocity: ~d~%" chan note vel)
+       ;;(format t "[NOTE-OFF] Channel: ~d  Notenum: ~d  Velocity: ~d~%" chan note vel)
        (queues:qpush event-queue (cl-synthesizer-midi-event:make-note-off-event chan note vel))))
     (list
      :inputs (lambda () '())
