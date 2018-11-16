7427335e1d2f27606ad05d2bbc393bc39ceb749c
Oliver Voeltz <frechmatz@gmx.de>
2018-11-16 18:52:19 +0100

    Added Wave-File-Monitor

--- src/monitor/monitor/example-2.lisp
+++ src/monitor/monitor/example-2.lisp
@@ -11,7 +11,16 @@
      "VCO"
      #'cl-synthesizer-modules-linear-vco:make-module
      :base-frequency 10 :v-peak 5 :cv-max 5 :f-max 12000)
-    
+
+    (cl-synthesizer-monitor:add-monitor
+     rack
+     #'cl-synthesizer-monitor-wave-handler:wave-file-handler
+     '((:channel-1 "VCO" :output-socket :sine)
+       (:channel-2 "VCO" :output-socket :square)
+       (:channel-3 "VCO" :output-socket :triangle)
+       (:channel-4 "VCO" :output-socket :saw))
+     :filename "waves/monitor-example-2.wav")
+
     (cl-synthesizer-monitor:add-monitor
      rack
      #'cl-synthesizer-monitor-csv-handler:csv-file-handler
@@ -22,7 +31,7 @@
      :filename "waves/monitor-example-2.csv"
      :add-header t
      :column-separator ",")
-
+    
     rack))
       
 ;;(cl-synthesizer:play-rack (example) 1)
