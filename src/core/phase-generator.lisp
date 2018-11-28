f24fd4db51ca18d4008f2877115e18f649b75029
Oliver Voeltz <frechmatz@gmx.de>
2018-11-28 20:24:50 +0100

    Bugfixes and support of negative frequencies

--- src/core/phase-generator.lisp
+++ src/core/phase-generator.lisp
@@ -1,19 +1,20 @@
 (in-package :cl-synthesizer-core)
 
+(defconstant 2PI (* 2 PI))
+
 (defun phase-generator (sample-rate)
-  "Generates a phase 0 >= phi < 2pi depending on frequency and sample-rate
+  "Generates a phase 0 <= Phi < 2PI depending on frequency and sample-rate
    Returns a function that has the following parameters:
-   - frequency -- the current frequency
-   The function returns the current phase."
-  (let ((cur-phi 0))
+   - frequency -- the current frequency. If the frequency is negative then phi will
+     move in clockwise (backward) direction.
+   Returns the current phase."
+  (let ((phi 0))
     (flet ((get-delta-phi (frequency)
-	     (let ((deltaPhi (/ (* 2 PI frequency) sample-rate)))
-	       deltaPhi)))
+	     (/ (* 2PI frequency) sample-rate)))
       (lambda (frequency)
-	(let ((r cur-phi))
-	  (setf cur-phi (+ cur-phi (get-delta-phi frequency)))
-	  (if (> cur-phi (* 2 PI))
-	      ;; 'reset' cur-phi to avoid overflow and for maximum precision
-	      (setf cur-phi (- cur-phi (* 2 PI))))
-	  r)))))
+	(setf phi (rem (+ phi (get-delta-phi frequency)) 2PI))
+	(if (> 0.0 phi)
+	    (+ 2PI phi) ;; -45 deg => 315 deg
+	    phi)))))
+
 
