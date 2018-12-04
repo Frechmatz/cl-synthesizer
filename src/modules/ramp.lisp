aa67afa42ca91822c2d9efe149c4d344c2873a16
Oliver Voeltz <frechmatz@gmx.de>
2018-12-04 22:00:54 +0100

    Implementation Ramp module

--- src/modules/ramp.lisp
+++ src/modules/ramp.lisp
@@ -4,15 +4,18 @@
 
 (in-package :cl-synthesizer-modules-ramp)
 
-(defun make-module (name environment &key time-ms target-value)
+(defun make-module (name environment &key time-ms target-output)
   "TODO"
-  (declare (ignore environment time-ms target-value))
-  (let ((output 0.0) (busy 0.0) (done 0.0))
+  ;;(declare (optimize (debug 3) (speed 0) (space 0)))
+  (let* ((output 0.0) (busy 0.0) (done 0.0) (elapsed-time-ms 0.0)
+	 (start 0.0)
+	 (sample-rate (getf environment :sample-rate))
+	 (tick-delta-ms (/ 1 (/ sample-rate 1000.0))))
     (list
-     :inputs (lambda () '(:trigger :input))
+     :inputs (lambda () '(:trigger :input :pass-through))
      ;; :busy -> Gate
      ;; :done -> Trigger
-     ;; we need bot :busy and :done due to retriggering.
+     ;; we need both :busy and :done due to retriggering.
      :outputs (lambda () '(:output :busy :done))
      :get-output (lambda (output-socket)
 		   (cond
@@ -24,8 +27,40 @@
 		      done)
 		     (t
 		      (error (format nil "Output socket ~a not supported by module ~a" output-socket name)))))
-     :update (lambda (&key trigger input)
-	       (declare (ignore trigger input))
-	       nil))))
+     :update (lambda (&key trigger input pass-through)
+	       ;;(declare (optimize (debug 3) (speed 0) (space 0)))
+	       (setf done 0.0)
+	       (if (not input)
+		   (setf input 0.0))
+	       (if (not trigger)
+		   (setf trigger 0.0))
+	       (if (not pass-through)
+		   (setf pass-through 0.0))
+	       (if (> pass-through 0.0)
+		   (setf output input)
+		   (progn 
+		     (if (> trigger 0.0)
+			 ;; Start ramp
+			 (progn
+			   (setf busy 5.0)
+			   (setf start input) ;; sample
+			   (setf output input)
+			   (setf elapsed-time-ms 0.0)))
+		     ;; Continue only when busy
+		     (if (> busy 0.0)
+			 (if (<= time-ms 0.0)
+			     (progn
+			       (setf done 5.0)
+			       (setf busy 0.0))
+			     (progn
+			       (setf elapsed-time-ms (+ elapsed-time-ms tick-delta-ms))
+			       (if (> elapsed-time-ms time-ms)
+				   (progn
+				     (setf done 5.0)
+				     (setf busy 0.0))
+				   (let* ((ticks-per-cycle (* time-ms (/ sample-rate 1000)))
+					  (delta (/ (- target-output start) ticks-per-cycle)))
+				     (setf output (+ output delta)))))))))))))
+
 
 
--- src/modules/ramp/example-1.lisp
+++ src/modules/ramp/example-1.lisp
@@ -18,25 +18,38 @@
      #'cl-synthesizer-modules-cv-to-trigger:make-module
      :trigger-cv 4.9 :pulse-voltage 5.0)
 
+    ;; Attack
     (cl-synthesizer:add-module
-     rack "RAMP"
+     rack "RAMP-1"
      #'cl-synthesizer-modules-ramp:make-module
-     :time-ms 500 :target-value 3.0)
+     :time-ms 200 :target-output 4.9)
+
+    ;; Decay
+    (cl-synthesizer:add-module
+     rack "RAMP-2"
+     #'cl-synthesizer-modules-ramp:make-module
+     :time-ms 200 :target-output 3.0)
     
     (cl-synthesizer:add-patch rack "LFO" :square "TRIGGER" :input)
-    (cl-synthesizer:add-patch rack "TRIGGER" :output "RAMP" :trigger)
+    (cl-synthesizer:add-patch rack "TRIGGER" :output "RAMP-1" :trigger)
+
+    (cl-synthesizer:add-patch rack "RAMP-1" :busy "RAMP-2" :pass-through)
+    (cl-synthesizer:add-patch rack "RAMP-1" :output "RAMP-2" :input)
+    (cl-synthesizer:add-patch rack "RAMP-1" :done "RAMP-2" :trigger)
     
     (cl-synthesizer-monitor:add-monitor
      rack
      #'cl-synthesizer-monitor-wave-handler:make-handler
      '(("LFO" :output-socket :square)
-       ("TRIGGER" :output-socket :output)
-       ("RAMP" :output-socket :output)
-       ("RAMP" :output-socket :busy)
-       ("RAMP" :output-socket :done))
+       ;;("TRIGGER" :output-socket :output)
+       ("RAMP-1" :output-socket :output)
+       ("RAMP-2" :output-socket :output)
+       ;;("RAMP-1" :output-socket :busy)
+       ;;("RAMP-1" :output-socket :done)
+       )
      :filename "waves/ramp-example-1.wav")
     
     
     rack))
 
-;;(cl-synthesizer:play-rack (example) 5)
+;;(cl-synthesizer:play-rack (example) 1)
