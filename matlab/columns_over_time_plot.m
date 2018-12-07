95faa28135c6eb15f82b886c349ed717757b570e
Oliver Voeltz <frechmatz@gmx.de>
2018-12-07 21:21:22 +0100

    Multi column plot script

---
+++ matlab/columns_over_time_plot.m
@@ -0,0 +1,15 @@
+% Plots all columns (y) over time (x)
+% Input is a CSV file with n columns and a header row
+content = importdata('/Users/olli/waves/ramp-example-1.csv');
+sampleRate = 44100;
+% Create X-Axis
+ticks = 0:1:size(content.data)-1;
+ticks = ticks / sampleRate;
+columnCount = length(content.colheaders);
+figure
+for index = 1:columnCount
+    subplot(columnCount,1,index);
+    plot(ticks,content.data(:, index));
+    xlabel('t (s)');
+    ylabel(content.colheaders(index));
+end
