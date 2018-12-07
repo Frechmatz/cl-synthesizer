f94ac782cd0c612f3722fe7e33097226d436618b
Oliver Voeltz <frechmatz@gmx.de>
2018-12-07 21:38:18 +0100

    Ramp Stage

--- src/modules/ramp.lisp
+++ src/modules/ramp.lisp
@@ -4,23 +4,27 @@
 
 (in-package :cl-synthesizer-modules-ramp)
 
-(defun make-module (name environment &key time-ms target-output)
+(defun make-module (name environment &key time-ms target-output (gate-state nil))
   "TODO
    It cannot be guaranteed that target-output will be exactly reached. 
    Due to the increments calculated out of time-ms and sample-rate 
    the ramp may stop at an output value a little bit smaller or 
    greater than the desired target-output value."
   ;;(declare (optimize (debug 3) (speed 0) (space 0)))
+  (if (and gate-state (not (eq gate-state :on)) (not (eq gate-state :off)))
+      (cl-synthesizer:signal-assembly-error
+       :format-control "~a: Invalid gate-state ~a Must be one of nil, :on, :off"
+       :format-arguments (list name gate-state)))
   (let* ((output 0.0) (busy 0.0) (done 0.0) (elapsed-time-ms 0.0)
-	 (start 0.0)
+	 (start 0.0) (cur-gate 0.0)
 	 (sample-rate (getf environment :sample-rate))
 	 (tick-delta-ms (/ 1 (/ sample-rate 1000.0))))
     (list
-     :inputs (lambda () '(:trigger :input :pass-through))
+     :inputs (lambda () '(:trigger :input :pass-through :gate))
      ;; :busy -> Gate
      ;; :done -> Trigger
      ;; we need both :busy and :done due to retriggering.
-     :outputs (lambda () '(:output :busy :done))
+     :outputs (lambda () '(:output :busy :done :gate))
      :get-output (lambda (output-socket)
 		   (cond
 		     ((eq :output output-socket)
@@ -29,11 +33,16 @@
 		      busy)
 		     ((eq :done output-socket)
 		      done)
+		     ((eq :gate output-socket)
+		      cur-gate)
 		     (t
 		      (error (format nil "Output socket ~a not supported by module ~a" output-socket name)))))
-     :update (lambda (&key trigger input pass-through)
+     :update (lambda (&key trigger input pass-through gate)
 	       ;;(declare (optimize (debug 3) (speed 0) (space 0)))
 	       (setf done 0.0)
+	       (if (not gate)
+		   (setf gate 0.0))
+	       (setf cur-gate gate)
 	       (if (not input)
 		   (setf input 0.0))
 	       (if (not trigger)
@@ -50,9 +59,11 @@
 			   (setf start input) ;; sample
 			   (setf output input)
 			   (setf elapsed-time-ms 0.0)))
-		     ;; Continue only when busy
+		     ;; Only continue when busy
 		     (if (> busy 0.0)
-			 (if (<= time-ms 0.0)
+			 (if (or (<= time-ms 0.0)
+				 (and gate-state (eq gate-state :on) (<= cur-gate 0.0))
+				 (and gate-state (eq gate-state :off) (> 0.0 cur-gate)))
 			     (progn
 			       (setf done 5.0)
 			       (setf busy 0.0))
--- src/modules/ramp/example-1.lisp
+++ src/modules/ramp/example-1.lisp
@@ -11,7 +11,7 @@
     (cl-synthesizer:add-module
      rack "LFO"
      #'cl-synthesizer-modules-vco:make-module
-     :base-frequency 0.25 :v-peak 5.0 :f-max 500 :cv-max 5)
+     :base-frequency 0.5 :v-peak 5.0 :f-max 500 :cv-max 5)
 
     (cl-synthesizer:add-module
      rack "TRIGGER"
@@ -22,13 +22,13 @@
     (cl-synthesizer:add-module
      rack "RAMP-1"
      #'cl-synthesizer-modules-ramp:make-module
-     :time-ms 200 :target-output 4.9)
+     :time-ms 200 :target-output 5.0 :gate-state nil)
 
     ;; Decay
     (cl-synthesizer:add-module
      rack "RAMP-2"
      #'cl-synthesizer-modules-ramp:make-module
-     :time-ms 200 :target-output 3.0)
+     :time-ms 200 :target-output 2.5)
     
     (cl-synthesizer:add-patch rack "LFO" :square "TRIGGER" :input)
     (cl-synthesizer:add-patch rack "TRIGGER" :output "RAMP-1" :trigger)
@@ -36,20 +36,16 @@
     (cl-synthesizer:add-patch rack "RAMP-1" :busy "RAMP-2" :pass-through)
     (cl-synthesizer:add-patch rack "RAMP-1" :output "RAMP-2" :input)
     (cl-synthesizer:add-patch rack "RAMP-1" :done "RAMP-2" :trigger)
-    
+
     (cl-synthesizer-monitor:add-monitor
      rack
-     #'cl-synthesizer-monitor-wave-handler:make-handler
-     '(("LFO" :output-socket :square)
-       ;;("TRIGGER" :output-socket :output)
-       ("RAMP-1" :output-socket :output)
-       ("RAMP-2" :output-socket :output)
-       ;;("RAMP-1" :output-socket :busy)
-       ;;("RAMP-1" :output-socket :done)
-       )
-     :filename "waves/ramp-example-1.wav")
+     #'cl-synthesizer-monitor-csv-handler:make-handler
+     '(("LFO" :output-socket :square :name "LFO Out" :format "~,5F")
+       ("RAMP-1" :output-socket :output :name "Ramp 1 Out" :format "~,5F")
+       ("RAMP-2" :output-socket :output :name "Ramp 2 Out" :format "~,5F"))
+     :filename "waves/ramp-example-1.csv")
     
     
     rack))
 
-;;(cl-synthesizer:play-rack (example) 1)
+;;(cl-synthesizer:play-rack (example) 5)
