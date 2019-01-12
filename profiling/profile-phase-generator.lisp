6054e5b9eaa62bda3d1571b476c9af4503ab0cce
Oliver Voeltz <frechmatz@gmx.de>
2019-01-12 01:18:33 +0100

    Code cleanup

--- profiling/profile-phase-generator.lisp
+++ profiling/profile-phase-generator.lisp
@@ -3,11 +3,6 @@
 
 (in-package :cl-synthesizer-profiling-phase-generator)
 
-(defparameter *sample-rate* 44100.0)
-
-(defun get-description ()
-  (format nil "Running a rack with ~a VCOs" *vco-count*))
-
 (defun run-phase-generator (&key duration-seconds)
   (let ((pg (cl-synthesizer-core:phase-generator 44100.0))
 	(phi 0))
