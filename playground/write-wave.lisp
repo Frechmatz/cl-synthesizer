5d47b26762b0b757892a1eed4b9728dcef1d9925
Oliver Voeltz <frechmatz@gmx.de>
2019-02-19 21:33:08 +0100

    Wave-Writing

--- playground/write-wave.lisp
+++ playground/write-wave.lisp
@@ -3,46 +3,37 @@
 
 (in-package :cl-synthesizer-playground-write-wave)
 
+
+(defun write-stereo-samples (writer value)
+  (dotimes (i 44100)
+    (funcall (getf writer :write-sample) value)
+    (funcall (getf writer :write-sample) value)))
+
 (defun write-test-files ()
-  (let ((writer (cl-synthesizer-modules-wave-file-writer::make-writer
-	       :filename "/Users/olli/cl-synthesizer-examples/test16Bit.wav"
-	       :channel-count 2
-	       :sample-rate 44100
-	       :sample-width :16Bit)))
-    (funcall (getf writer :open-file))
-    (dotimes (i 44100)
-      (funcall (getf writer :write-sample) 0.9)
-      (funcall (getf writer :write-sample) 0.9)
-      )
-    (dotimes (i 44100)
-      (funcall (getf writer :write-sample) -0.9)
-      (funcall (getf writer :write-sample) -0.9)
-      )
-    (dotimes (i 44100)
-      (funcall (getf writer :write-sample) 0.9)
-      (funcall (getf writer :write-sample) 0.9)
-      )
-    (funcall (getf writer :close-file)))
 
   (let ((writer (cl-synthesizer-modules-wave-file-writer::make-writer
-	       :filename "/Users/olli/cl-synthesizer-examples/test8Bit.wav"
+	       :filename "/Users/olli/src/lisp/cl-synthesizer/playground/test8Bit.wav"
 	       :channel-count 2
 	       :sample-rate 44100
 	       :sample-width :8Bit)))
     (funcall (getf writer :open-file))
-    (dotimes (i 44100)
-      (funcall (getf writer :write-sample) 0.9)
-      (funcall (getf writer :write-sample) 0.9)
-      )
-    (dotimes (i 44100)
-      (funcall (getf writer :write-sample) -0.9)
-      (funcall (getf writer :write-sample) -0.9)
-      )
-    (dotimes (i 44100)
-      (funcall (getf writer :write-sample) 0.9)
-      (funcall (getf writer :write-sample) 0.9)
-      )
+    (funcall (getf writer :open-file))
+    (write-stereo-samples writer 1.0)
+    (write-stereo-samples writer -1.0)
+    ;;(write-stereo-samples writer -1.0)
     (funcall (getf writer :close-file)))
+  
+  (let ((writer (cl-synthesizer-modules-wave-file-writer::make-writer
+	       :filename "/Users/olli/src/lisp/cl-synthesizer/playground/test16Bit.wav"
+	       :channel-count 2
+	       :sample-rate 44100
+	       :sample-width :16Bit)))
+    (funcall (getf writer :open-file))
+    (write-stereo-samples writer 1.0)
+    (write-stereo-samples writer -1.0)
+    ;; (write-stereo-samples writer -1.0)
+    (funcall (getf writer :close-file)))
+
 
   "DONE")
   
