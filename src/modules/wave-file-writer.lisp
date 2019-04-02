95574d0d13a9aaad5f4f51a2b89909ad6966e040
Oliver Voeltz <frechmatz@gmx.de>
2019-04-02 21:22:39 +0200

    Switched over to v2 interface

--- src/modules/wave-file-writer.lisp
+++ src/modules/wave-file-writer.lisp
@@ -214,27 +214,34 @@
       (cl-synthesizer:signal-assembly-error
        :format-control "~a: channel-count must be greater than 0: ~a"
        :format-arguments (list name channel-count)))
-  (let ((inputs (cl-synthesizer-macro-util:make-keyword-list "channel" channel-count))
+  (let ((input-sockets (cl-synthesizer-macro-util:make-keyword-list "channel" channel-count))
+	(input-values (make-array channel-count))
 	(opened-wave-writer nil)
 	(wave-writer (funcall *make-writer*
 		      :filename (merge-pathnames filename (getf environment :home-directory))
 		      :channel-count channel-count
 		      :sample-width sample-width
 		      :sample-rate (floor (getf environment :sample-rate)))))
-    ;; inputs are now (:CHANNEL-1 ... :CHANNEL-n)
+    ;; input-sockets are now (:CHANNEL-1 ... :CHANNEL-n)
+    (let ((inputs nil) (index 0))
+      (dolist (input-socket input-sockets)
+	(let ((cur-index index))
+	  (push (lambda(value) (setf (aref input-values cur-index) value)) inputs)
+	  (push input-socket inputs)
+	  (setf index (+ 1 index))))
     (list
+     :v2 t
      :inputs (lambda () inputs)
-     :outputs (lambda () '())
-     :get-output (lambda (output) (declare (ignore output)) nil)
-     :update (lambda (args)
+     :outputs (lambda () nil)
+     :update (lambda ()
 	       (if (not opened-wave-writer)
 		   (progn
 		     (setf opened-wave-writer t)
 		     (funcall (getf wave-writer :open-file))))
-	       (dolist (input inputs)
-		 (let ((value (getf args input)))
+	       (dotimes (i channel-count)
+		 (let ((value (aref input-values i)))
 		   (if (not value)
 		       (setf value 0.0))
 		   (funcall (getf wave-writer :write-sample) (input-to-wave value v-peak)))))
      :shutdown (lambda ()
-		 (funcall (getf wave-writer :close-file))))))
+		 (funcall (getf wave-writer :close-file)))))))
