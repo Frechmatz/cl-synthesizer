bebaac4f8435cc6d8819d0428fd60a9468b62d3e
Oliver Voeltz <frechmatz@gmx.de>
2019-12-04 20:40:56 +0100

    Fixed frequencies

--- patches/harmonic.lisp
+++ patches/harmonic.lisp
@@ -8,83 +8,43 @@
 	       :environment (cl-synthesizer:make-environment :sample-rate sample-rate)
 	       :output-sockets '(:audio))))
 
-    (cl-synthesizer:add-module
-     rack "MIXER" #'cl-synthesizer-modules-mixer:make-module
-     :channel-count 9 :channel-cv-max 5.0 :channel-cv-gain 5.0 :main-cv-max 5.0 :main-cv-gain 5.0)
-    
-    (let ((fundamental 440))
+    (let ((f-first-harmonic 220.0))
+      (let ((waves
+	     `((:base-frequency ,f-first-harmonic :v-peak-rel 0.2)
+	       (:base-frequency ,(* 2.0 f-first-harmonic) :v-peak-rel 0.37)
+	       (:base-frequency ,(* 4.0 f-first-harmonic) :v-peak-rel 1.0)
+	       (:base-frequency ,(* 6.0 f-first-harmonic) :v-peak-rel 0.12)
+	       (:base-frequency ,(* 8.0 f-first-harmonic) :v-peak-rel 0.15))))
 
-      (cl-synthesizer:add-module
-       rack "VCO-0"
-       #'cl-synthesizer-modules-vco:make-module
-       :base-frequency fundamental :v-peak 5.0 :wave-forms '(:sine))
-      (cl-synthesizer:add-patch rack "VCO-0" :sine "MIXER" :channel-1)
+	(let ((main-cv-gain 2.5)) ;; war 5.0 (runterdrehen um Clipping zu verhindern)
+	  (cl-synthesizer:add-module
+	   rack "MIXER" #'cl-synthesizer-modules-mixer:make-module
+	   :channel-count (length waves) :channel-cv-max 5.0 :channel-cv-gain 5.0 :main-cv-max 5.0 :main-cv-gain main-cv-gain))
+	
+	(let ((index 0))
+	  (dolist (osc waves)
+	    (let ((vco-name (format nil "VCO-~a" index)))
+		  (cl-synthesizer:add-module
+		   rack vco-name
+		   #'cl-synthesizer-modules-vco:make-module
+		   :base-frequency (getf osc :base-frequency) :v-peak (* 5.0 (getf osc :v-peak-rel)) :wave-forms '(:sine))
+		  (cl-synthesizer:add-patch rack vco-name :sine "MIXER" (cl-synthesizer-macro-util:make-keyword "channel" index))
+		  (setf index (+ 1 index))))))
 
-      (cl-synthesizer:add-module
-       rack "VCO-1"
-       #'cl-synthesizer-modules-vco:make-module
-       :base-frequency (* 2 fundamental) :v-peak 5.0 :wave-forms '(:sine))
-      (cl-synthesizer:add-patch rack "VCO-1" :sine "MIXER" :channel-2)
-
-      (cl-synthesizer:add-module
-       rack "VCO-2"
-       #'cl-synthesizer-modules-vco:make-module
-       :base-frequency (* 3 fundamental) :v-peak 5.0 :wave-forms '(:sine))
-      (cl-synthesizer:add-patch rack "VCO-2" :sine "MIXER" :channel-3)
-      
-      (cl-synthesizer:add-module
-       rack "VCO-3"
-       #'cl-synthesizer-modules-vco:make-module
-       :base-frequency (* 4 fundamental) :v-peak 5.0 :wave-forms '(:sine))
-      (cl-synthesizer:add-patch rack "VCO-3" :sine "MIXER" :channel-4)
-
-      (cl-synthesizer:add-module
-       rack "VCO-4"
-       #'cl-synthesizer-modules-vco:make-module
-       :base-frequency (* 5 fundamental) :v-peak 5.0 :wave-forms '(:sine))
-      (cl-synthesizer:add-patch rack "VCO-4" :sine "MIXER" :channel-5)
-
-      (cl-synthesizer:add-module
-       rack "VCO-5"
-       #'cl-synthesizer-modules-vco:make-module
-       :base-frequency (* 6 fundamental) :v-peak 5.0 :wave-forms '(:sine))
-      (cl-synthesizer:add-patch rack "VCO-5" :sine "MIXER" :channel-6)
-      
-      (cl-synthesizer:add-module
-       rack "VCO-6"
-       #'cl-synthesizer-modules-vco:make-module
-       :base-frequency (* 7 fundamental) :v-peak 5.0 :wave-forms '(:sine))
-      (cl-synthesizer:add-patch rack "VCO-6" :sine "MIXER" :channel-7)
-      
-      (cl-synthesizer:add-module
-       rack "VCO-7"
-       #'cl-synthesizer-modules-vco:make-module
-       :base-frequency (* 8 fundamental) :v-peak 5.0 :wave-forms '(:sine))
-      (cl-synthesizer:add-patch rack "VCO-7" :sine "MIXER" :channel-8)
-
-      (cl-synthesizer:add-module
-       rack "VCO-8"
-       #'cl-synthesizer-modules-vco:make-module
-       :base-frequency (* 9 fundamental) :v-peak 5.0 :wave-forms '(:sine))
-      (cl-synthesizer:add-patch rack "VCO-8" :sine "MIXER" :channel-9)
-
-      
       (cl-synthesizer:add-patch rack "MIXER" :output "OUTPUT" :audio)
 
-      #|
       (cl-synthesizer-monitor:add-monitor
       rack
       #'cl-synthesizer-monitor-wave-handler:make-handler
       '(("OUTPUT" :input-socket :audio))
       :filename "src/lisp/cl-synthesizer/docs/harmonic.wav")
-      |#
 
       (cl-synthesizer-monitor:add-monitor
        rack
        #'cl-synthesizer-monitor-csv-handler:make-handler
-       '(("OUTPUT" :input-socket :audio))
-       :filename "src/lisp/cl-synthesizer/docs/harmonic.wav")
-
+       '(("MIXER" :output-socket :output :name "Mixer-Output"))
+       :filename "src/lisp/cl-synthesizer/docs/harmonic.csv")
+      
       rack)))
 
 (defparameter *duration-seconds* 4.0)
