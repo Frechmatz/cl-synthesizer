819429c83eb03f27d8a2f4397843dbfe68eb58a3
Oliver Voeltz <frechmatz@gmx.de>
2020-12-30 19:52:26 +0100

    milligram.css

--- makedoc/make-doc.lisp
+++ makedoc/make-doc.lisp
@@ -105,7 +105,11 @@
 
 (defun get-readme (lib-index readme-index)
   `("<html>"
-    "<head><link href=\"styles.css\" rel=\"stylesheet\" type=\"text/css\"/></head>"
+    "<head><link href=\"styles.css\" rel=\"stylesheet\" type=\"text/css\"/>"
+    "<link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic\">"
+    "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css\">"
+    "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css\">"
+    "</head>"
     "<body>"
     (semantic (:name "header")
 	      (heading
